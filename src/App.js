52ebe304dbeb802645f891c187cece7cbb4ff198
rekbrjdf <vv_kulikova@betboom.org>
2022-02-20 23:54:06 +0300

    Add project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Cards from './components/Cards';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+       <Cards></Cards>
+    </div>
+  );
+}
+
+export default App;
